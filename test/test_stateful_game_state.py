89f2a8d50d184c8c46e0fde8e6f2429764ea2159
matt lane <mattjmlane@gmail.com>
2017-02-01 11:07:17 +0000

    Some basic stateful_game_state unit tests

--- test/test_stateful_game_state.py
+++ test/test_stateful_game_state.py
@@ -1,22 +1,57 @@
 #!/usr/bin/env python3
 
 import nose
+from nose.tools import raises
 import os
 import uuid
 import sys
 
 # Allow relative imports of the parent modules
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+import game_state
 import stateful_game_state
 
-def setup_func():
-    print('set up test fixtures')
+def create_gs(config):
+    return stateful_game_state.StatefulGameState({})
 
-def teardown_func():
-    print('tear down test fixtures')
-
-@nose.with_setup(setup_func, teardown_func)
 def test_add_user():
-    gs = stateful_game_state.StatefulGameState({})
-    gs.add_user(uuid.uuid4())
-    print(gs.state)
+    create_gs({}).add_user(uuid.uuid4())
+
+def test_remove_user():
+    gs = create_gs({})
+    id = uuid.uuid4()
+    gs.add_user(id)
+    gs.remove_user(id)
+
+@raises(game_state.UserDoesntExistError)
+def test_remove_user_twice():
+    gs = create_gs({})
+    id = uuid.uuid4()
+    gs.add_user(id)
+    gs.remove_user(id)
+    gs.remove_user(id)
+
+@raises(game_state.UserAlreadyExistsError)
+def test_add_user_twice():
+    gs = create_gs({})
+    id = uuid.uuid4()
+    gs.add_user(id)
+    gs.add_user(id)
+
+def test_double_add_remove_user():
+    gs = create_gs({})
+    id = uuid.uuid4()
+    gs.add_user(id)
+    gs.remove_user(id)
+    gs.add_user(id)
+    gs.remove_user(id)
+
+def test_add_user_string_uuid():
+    gs = create_gs({})
+    gs.add_user(str(uuid.uuid4()))
+
+def test_remove_user_str():
+    gs = create_gs({})
+    id = uuid.uuid4()
+    gs.add_user(id)
+    gs.remove_user(str(id))
